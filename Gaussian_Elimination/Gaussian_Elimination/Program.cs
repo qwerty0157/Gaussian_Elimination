034be50bbb23e60cceb0f4ae19544ea9d8476d29
qwerty0157 <qwerty0157@gmail.com>
2018-08-04 18:53:08 +0900

    add calc matrix function

--- Gaussian_Elimination/Gaussian_Elimination/Program.cs
+++ Gaussian_Elimination/Gaussian_Elimination/Program.cs
@@ -6,7 +6,36 @@ namespace Gaussian_Elimination
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalcMatrixTest();
+        }
+
+        public static void CalcMatrixTest()
+        {
+            const int Dimension = 4;
+            double[,] matrixA = new double[Dimension, Dimension]
+            {
+                {2, 3, 1, 4},
+                {4, 1, -3, -2},
+                {-1, 2, 2, 1},
+                {3, -4, 4, 3}
+            };
+            double[] vectorB = new double[Dimension]
+            {
+                10,
+                0,
+                4,
+                6
+            };
+            double[] InitialSolution = new double[Dimension]
+            {
+                0,
+                0,
+                0,
+                0
+            };
+
+            var calcMatrix = new Solve_equation(Dimension, matrixA, vectorB, InitialSolution);
+            calcMatrix.solve();
         }
     }
 }
---
+++ Gaussian_Elimination/Gaussian_Elimination/Solve_equation.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Gaussian_Elimination
+{
+    public class Solve_equation
+    {
+        readonly int dimension;
+        readonly double[,] leftMatrix;
+        readonly double[] rightVector;
+        double[] solutionX;
+
+        public Solve_equation(int Dimension, double[,] LeftMatrix, double[] RightVector, double[] SolutionX)
+        {
+            this.dimension = Dimension;
+            this.leftMatrix = LeftMatrix;
+            this.rightVector = RightVector;
+            this.solutionX = SolutionX;
+        }
+
+        public void solve()
+        {
+            BackwardSubstitution(ForwardElimination());
+        }
+
+        private void WriteEquationFuntion()
+        {
+            var rowCount = rightVector.Length;
+            if(rowCount!= leftMatrix.GetLength(0))
+            {
+                throw new Exception();
+            }
+
+            for (int i = 0; i < rowCount; ++i)
+            {
+                for (int j = 0; j < leftMatrix.GetLength(1); ++j)
+                {
+                    Console.Write($"{leftMatrix[i, j],8:F4}");
+                }
+                Console.WriteLine($" | {rightVector[i],8:F4}");
+            }
+        }
+
+        private void WriteEquationFuntion(double[,] LeftMatrix, double[] RightVector)
+        {
+            var rowCount = RightVector.Length;
+            if (rowCount != LeftMatrix.GetLength(0))
+            {
+                throw new Exception();
+            }
+
+            for (int i = 0; i < rowCount; ++i)
+            {
+                for (int j = 0; j < LeftMatrix.GetLength(1); ++j)
+                {
+                    Console.Write($"{LeftMatrix[i, j],8:F4}");
+                }
+                Console.WriteLine($" | {RightVector[i],8:F4}");
+            }
+        }
+
+        private Tuple<double[,], double[]> ForwardElimination()
+        {
+            Console.WriteLine("Initial");
+            WriteEquationFuntion();
+            Console.WriteLine();
+
+            var matrixA = leftMatrix;
+            var vectorB = rightVector;
+            for (int i = 0; i < dimension - 1; ++i)
+            {
+                for (int j = i + 1; j < dimension; ++j)
+                {
+                    var s = matrixA[j, i] / matrixA[i, i];
+                    for (int k = i; k < dimension; ++k)
+                    {
+                        matrixA[j, k] -= matrixA[i, k] * s;
+                    }
+                    vectorB[j] -= vectorB[i] * s;
+                }
+            }
+            Console.WriteLine("After forward elimination");
+            WriteEquationFuntion(leftMatrix, vectorB);
+            Console.WriteLine();
+
+            var result = new Tuple<double[,], double[]>(matrixA, vectorB);
+            return result;
+        }
+
+        private void BackwardSubstitution(Tuple<double[,], double[]> forwardMatrix)
+        {
+            for (int i = dimension - 1; i >= 0; --i)
+            {
+                var vec = forwardMatrix.Item2;
+                var mat = forwardMatrix.Item1;
+                var s = vec[i];
+
+                for (int j = i + 1; j < dimension; ++j)
+                {
+                    s -= mat[i, j] * solutionX[j];
+                }
+                solutionX[i] = s / mat[i, i];
+            }
+            Console.WriteLine("After Backward Substitution");
+            WriteEquationFuntion(forwardMatrix.Item1, forwardMatrix.Item2);
+            Console.WriteLine();
+
+            Console.WriteLine("Solved");
+            Console.WriteLine(string.Join("\r\n", solutionX.Select(x => $"{x,8:F4}")));
+        }
+
+    }
+}
